dratani/Introduccion_SNMP

Administración de contabilidad Agregado create, update y graph

4. AdministraciónDeContabilidad/CreateRRD.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import rrdtool
-ret = rrdtool.create("traficoRED.rrd",
+ret = rrdtool.create("segmentosRed.rrd",
                      "--start",'N',
                      "--step",'60',
-                     "DS:inoctets:COUNTER:120:U:U",
-                     "DS:outoctets:COUNTER:120:U:U",
+                     "DS:segmentosEntrada:COUNTER:120:U:U",
+                     "DS:segmentosSalida:COUNTER:120:U:U",
                      "RRA:AVERAGE:0.5:6:5",
                      "RRA:AVERAGE:0.5:1:20")
 

AxGrayson5/ASR

2-AdquisiciónSNMP/CreateRRD.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-
 import rrdtool
 ret = rrdtool.create("traficoRED.rrd",
                      "--start",'N',
                      "--step",'60',
-                     "DS:inoctets:COUNTER:600:U:U",
-                     "DS:outoctets:COUNTER:600:U:U",
+                     "DS:inoctets:COUNTER:120:U:U",
+                     "DS:outoctets:COUNTER:120:U:U",
                      "RRA:AVERAGE:0.5:6:5",
                      "RRA:AVERAGE:0.5:1:20")
 